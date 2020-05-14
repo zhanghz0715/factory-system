4e2012843b65894cfd9e6fe42ff6c278d6766cff
zhz <846786337.qq.com>
2020-05-14 12:06:38 +0800

    1.添加表的控制层

---
+++ src/main/java/com/factory/boot/controller/MouldController.java
@@ -0,0 +1,22 @@
+package com.factory.boot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import com.factory.boot.config.BaseController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 
+ * @since 2020-05-14
+ */
+@Controller
+@RequestMapping("/mould")
+public class MouldController extends BaseController {
+
+}
+
