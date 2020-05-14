16542fcc37f743f9268903f4b9ba463cf7757bb9
zhz <846786337.qq.com>
2020-05-14 19:14:14 +0800

    1.添加排产管理的方法

--- src/main/java/com/factory/boot/controller/ProductController.java
+++ src/main/java/com/factory/boot/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.factory.boot.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.stereotype.Controller;
+import com.factory.boot.config.AjaxJson;
 import com.factory.boot.config.BaseController;
+import com.factory.boot.config.ExceptionUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -14,9 +17,35 @@ import com.factory.boot.config.BaseController;
  * @author 
  * @since 2020-05-14
  */
-@Controller
+@RestController
 @RequestMapping("/product")
+@Slf4j
 public class ProductController extends BaseController {
+
+
+
+    @PostMapping("/page")
+    public AjaxJson getPage(Integer pageNo,Integer pageSize,String date,String typeId){
+        AjaxJson ajaxJson = new AjaxJson();
+        try {
+
+        } catch (Exception e) {
+            log.error(ExceptionUtil.getExceptionAllinformation(e, getClass().getName()));
+            return new AjaxJson("服务器错误，请稍后重试");
+        }
+        return ajaxJson;
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
